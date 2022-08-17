38125318f45441cfa8954ab2231bba935846f5f5
ptrv-dev <petrov99119@gmail.com>
2022-08-17 16:17:56 +0300

    Feedback section slider PC version done.

--- src/js/app.js
+++ src/js/app.js
@@ -11,11 +11,55 @@ items.forEach((element) => {
     })
 });
 
+// Feedback Slider
+// slider-feedback__button_disable
+const prevButton = document.getElementsByClassName('slider-feedback__button_left')[0];
+const nextButton = document.getElementsByClassName('slider-feedback__button_right')[0];
 
-// let els = document.getElementsByClassName('questions-item__title');
+const slides = document.querySelectorAll('.feedback-slide');
 
-// Array.prototype.forEach.call(els, function(el) {
-//     el.onclick = function() {
-//         el.parentNode.classList.toggle('questions-item_active');
-//     }
-// });
+let currentSlide = 0;
+
+
+
+function moveSlider() {
+    console.log(currentSlide);
+    slides.forEach((e) => {
+        e.style.transform = `translateX(calc(-${100*currentSlide}% - ${currentSlide * 24}px))`;
+    });
+    if (currentSlide <= 0) {
+        if(!prevButton.classList.contains('slider-feedback__button_disable')) {
+            prevButton.classList.add('slider-feedback__button_disable');
+        }
+    } else {
+        if(prevButton.classList.contains('slider-feedback__button_disable')) {
+            prevButton.classList.remove('slider-feedback__button_disable');
+        }
+    }
+    if (currentSlide >= slides.length - 1) {
+        if(!nextButton.classList.contains('slider-feedback__button_disable')) {
+            nextButton.classList.add('slider-feedback__button_disable');
+        }
+    } else {
+        if(nextButton.classList.contains('slider-feedback__button_disable')) {
+            nextButton.classList.remove('slider-feedback__button_disable');
+        }
+    }
+}
+
+moveSlider();
+
+prevButton.addEventListener('click', (e) => {
+    if (currentSlide <= 0) {
+        return false;
+    }
+    currentSlide --;
+    moveSlider();
+});
+nextButton.addEventListener('click', (e) => {
+    if (currentSlide >= slides.length - 1) {
+        return false;
+    }
+    currentSlide ++;
+    moveSlider();
+});
